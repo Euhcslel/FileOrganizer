c97957e49d864f8fd9c0acd489ae614a22b39f5c
Sophia <nptyglhf@gmail.com>
2026-09-15 23:45:26 +0500

    добавлена возможность отрисовки папки

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -8,6 +10,7 @@ import (
 	"strings"
 )
 
+// Обобщенные расширения файлов
 var categories = map[string]map[string]string{
 	"image": {},
 	"application": {
@@ -19,25 +22,38 @@ var categories = map[string]map[string]string{
 	"video": {},
 }
 
+
 func main() {
+	// Установка флагов
+	dir := flag.String("dir", "Downloads", "sortable directory")
+	preDraw := flag.Bool("predraw", false, "predraw scheme")
+	flag.Parse()
+
+	// Определение домашней директории
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		log.Printf("ошибка при получении домашней директории пользователя: %s", err.Error())
 		return
 	}
-	defaultDir := "Downloads"
+	path := filepath.Join(homeDir, *dir)
 
-	path := filepath.Join(homeDir, defaultDir)
+	// Чтение папки
 	files, err := os.ReadDir(path)
 	if err != nil {
 		log.Printf("ошибка при чтении директории: %s", err.Error())
 		return
 	}
+
+	// Переменная для хранения новой структуры папки
+	var structure = map[string][]string{}
+
+	// Цикл по файлам в папке
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
 
+		// Открытие файла (для чтения)
 		oldPath := filepath.Join(path, file.Name())
 		openedFile, err := os.Open(oldPath)
 		if err != nil {
@@ -45,7 +61,7 @@ func main() {
 			continue
 		}
 		
-		
+		// Чтение файла в буфер
 		buffer := make([]byte, 512)
 		n, err := openedFile.Read(buffer)
 		if err != nil {
@@ -55,10 +71,12 @@ func main() {
 		}
 		openedFile.Close()
 
+		// Определение расширения файла
 		ext := http.DetectContentType(buffer[:n])
 		parts := strings.Split(ext, "/")
 		ext = parts[0]
 
+		// Определение категории в зависимости от расширения
 		var category string
 		values, ok := categories[ext]
 		if !ok {
@@ -71,14 +89,33 @@ func main() {
 			category = ext
 		}
 
+		// Заполнение итоговой структуры папок
+		if *preDraw {
+			structure[category] = append(structure[category], file.Name())
+			continue
+		}
+
+		// Создание папки для хранения файлов определенной категории
 		categoryPath := filepath.Join(path, category)
 		if err := os.MkdirAll(categoryPath, 0755); err != nil {
 			log.Printf("ошибка при создании новой папки: %s", err.Error())
 		}
 
+		// Перемещение файла в указанную папку
 		newPath := filepath.Join(categoryPath, file.Name())
 		if err := os.Rename(oldPath, newPath); err != nil {
 			log.Printf("ошибка при перемещении файла %s: %s", file.Name(), err.Error())
 		}
+	}
+
+	if *preDraw {
+		for category, files := range structure {
+			fmt.Printf("\n%s/", category)
+	
+			for _, file := range files {
+				fmt.Printf("\n\t%s", file)
+			}
+		}
+		fmt.Println()
 	}
 }
